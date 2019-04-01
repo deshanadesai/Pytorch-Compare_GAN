ad34711d7dcc6ffe9506ac202281d678bf99cf42
Deshana Desai <desai.deshna@gmail.com>
2019-04-01 18:42:28 -0400

    Update loss_lib.py

--- loss_lib.py
+++ loss_lib.py
@@ -15,7 +15,8 @@ import torch.nn.functional as F
 import torch
 
 class EBGan:
-	def __init__(self, margin):
+	def __init__(self, lambda_pt, margin):
+		self.lambda_pt = lambda_pt
 		self.margin = margin
 		self.loss = nn.MSELoss()
 
@@ -27,18 +28,18 @@ class EBGan:
 		loss_pt = (torch.sum(similarity) - batch_size) / (batch_size * (batch_size - 1))
 		return loss_pt
 
-	def ebgan_gloss(recon_imgs, gen_imgs, img_embeddings):
-		g_loss = pixelwise_loss(recon_imgs, gen_imgs.detach()) + lambda_pt * pullaway_loss(img_embeddings)
+	def gloss(recon_imgs, gen_imgs, img_embeddings):
+		g_loss = pixelwise_loss(recon_imgs, gen_imgs.detach()) + self.lambda_pt * pullaway_loss(img_embeddings)
 		return g_loss
 
 
-	def ebgan_dloss(real_recon, real_imgs, fake_recon, gen_imgs, margin):
+	def dloss(real_recon, real_imgs, fake_recon, gen_imgs):
 		d_loss_real = pixelwise_loss(real_recon, real_imgs)
 		d_loss_fake = pixelwise_loss(fake_recon, gen_imgs.detach())
 
 		d_loss = d_loss_real
-		if (margin - d_loss_fake.data).item() > 0:
-			d_loss += margin - d_loss_fake
+		if (self.margin - d_loss_fake.data).item() > 0:
+			d_loss += self.margin - d_loss_fake
 
 		return d_loss
 
